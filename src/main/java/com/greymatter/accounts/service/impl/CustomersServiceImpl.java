2200b5d900ed3212fadb5c6b3d12367d70faa910
Jorge Garcia <61385320+JcGarciaMed@users.noreply.github.com>
2024-10-20 20:08:02 -0500

    Adding gateway server and filter changes

--- src/main/java/com/greymatter/accounts/service/impl/CustomersServiceImpl.java
+++ src/main/java/com/greymatter/accounts/service/impl/CustomersServiceImpl.java
@@ -31,7 +31,7 @@ public class CustomersServiceImpl implements ICustomersService {
      * @return Customer Details based on a given mobileNumber
      */
     @Override
-    public CustomerDetailsDto fetchCustomerDetails(String mobileNumber) {
+    public CustomerDetailsDto fetchCustomerDetails(String mobileNumber, String correlationId) {
         Customer customer = customerRepository.findByMobileNumber(mobileNumber).orElseThrow(
                 () -> new ResourceNotFoundException("Customer", "mobileNumber", mobileNumber)
         );
@@ -42,13 +42,13 @@ public class CustomersServiceImpl implements ICustomersService {
         CustomerDetailsDto customerDetailsDto = CustomerMapper.mapToCustomerDetailsDto(customer, new CustomerDetailsDto());
         customerDetailsDto.setAccountsDto(AccountsMapper.mapToAccountsDto(accounts, new AccountsDto()));
 
-        ResponseEntity<LoansDto> loansDtoResponseEntity = loansFeignClient.fetchLoanDetails(mobileNumber);
+        ResponseEntity<LoansDto> loansDtoResponseEntity = loansFeignClient.fetchLoanDetails(correlationId, mobileNumber);
         customerDetailsDto.setLoansDto(loansDtoResponseEntity.getBody());
 
-        ResponseEntity<CardsDto> cardsDtoResponseEntity = cardsFeignClient.fetchCardDetails(mobileNumber);
+        ResponseEntity<CardsDto> cardsDtoResponseEntity = cardsFeignClient.fetchCardDetails(correlationId, mobileNumber);
         customerDetailsDto.setCardsDto(cardsDtoResponseEntity.getBody());
 
-        ResponseEntity<PersonDto> personDtoResponseEntity = identityFeignClient.findById("12345678");
+        ResponseEntity<PersonDto> personDtoResponseEntity = identityFeignClient.findById("45555215");
         customerDetailsDto.setPersonDto(personDtoResponseEntity.getBody());
 
 
